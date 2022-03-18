630a48873b55e5ee1a76859066e2d29c4171b176
CaoTrungKien19082003 <101707396+CaoTrungKien19082003@users.noreply.github.com>
2022-03-18 19:24:33 +0700

    Add Export List Student Course

--- Source.cpp
+++ Source.cpp
@@ -294,3 +294,31 @@ void ChangePassword(Schoolyear* YearCur, teacher* Staff, int x, int NumOfStaff)
 	cout << "Your password was changed successfully." << endl;
 	txtColor(7);
 }
+void ExportListStudentCourse(Course* CourseHead)
+{
+	string CourseID;
+	ofstream output;
+	Course* CourseCur = CourseHead;
+	cout << "Input course ID: ";
+	getline(cin, CourseID);
+	output.open(CourseID + ".csv", ofstream::out);
+	while (CourseCur != nullptr && CourseCur->CourseID != CourseID)
+	{
+		CourseCur = CourseCur->pNext;
+	}
+
+	if (CourseCur != nullptr)
+	{
+		Data* InfoCur = CourseCur->DataOfStu;
+		int i = 1;
+		while (InfoCur != nullptr)
+		{
+			output << i << ',' << InfoCur->StudentID << ',' << InfoCur->Surname << ',' << InfoCur->Name << endl;
+			InfoCur = InfoCur->pNext;
+		}
+
+		if (InfoCur == nullptr) cout << "Export successfully!";
+		else cout << "Export fail";
+	}
+	output.close();
+}
