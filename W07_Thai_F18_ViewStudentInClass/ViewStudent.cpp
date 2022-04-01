d87e74c2a3e216424f3e808563fe8a98f7fe1ac1
ThaiCoder2003 <101780284+ThaiCoder2003@users.noreply.github.com>
2022-04-01 21:08:46 +0700

    W07_Thai_F18_ViewStudentInClass(testing incomplete)

--- W07_Thai_F18_ViewStudentInClass/ViewStudent.cpp
+++ W07_Thai_F18_ViewStudentInClass/ViewStudent.cpp
@@ -1,26 +1,51 @@
 #include "header.h"
 
 void ViewListStudentInClass(Schoolyear* YearCur){
-	string year, ClassID;
-	
-	cout << "Enter the year you want to search: ";
-	getline(cin, year);
-	while (YearCur!=nullptr){
-		YearCur=YearCur->YearNext;
-		
-		if (YearCur->NumOfYear==year){
-			cout 
-			for (int i=0; i<YearCur->NumOfClass; i++){
-				if ()
-			}
+	string ClassID;
+	cout << "Enter the class you want to show the student: ";
+	getline(cin, ClassID);
 			
+	for (int i=0; i<YearCur->NumOfClass; i++){
+			if (YearCur->CLass[i].NameOfClass==ClassID){
+				for (int j=0; j<YearCur->CLass[i].NumOfStudent; j++){
+					cout << YearCur->CLass[i].Stu[j].Num << "    ";
+					cout << YearCur->CLass[i].Stu[j].Surname << "    ";
+					cout << YearCur->CLass[i].Stu[j].Name << "    ";
+					cout << YearCur->CLass[i].Stu[j].DOB << "    ";
+					cout << YearCur->CLass[i].Stu[j].Gender << "    ";
+					cout << YearCur->CLass[i].Stu[j].StudentID << "    " << endl;					
+				}
+					
+				break;
+			}
 		}
-		
-		break;
-	}
 	
 	if (YearCur==nullptr){
 		cout << "Schoolyear not found!";
 	}
 	
+}
+
+int main(){
+	Schoolyear *YearCur=new Schoolyear;
+	ifstream output;
+	output.open("2021-2022.txt");
+	output >> YearCur->NumOfClass;
+	YearCur->CLass=new Class[YearCur->NumOfClass];
+	for (int i=0; i<YearCur->NumOfClass; i++){
+		output >> YearCur->CLass[i].NameOfClass;
+		output >> YearCur->CLass[i].NumOfStudent;
+		YearCur->CLass[i].Stu=new Student[YearCur->CLass[i].NumOfStudent];
+		for (int j=0; j<YearCur->CLass[i].NumOfStudent; j++){
+			output >> YearCur->CLass[i].Stu[j].Num;
+			output >> YearCur->CLass[i].Stu[j].Surname;
+			output >> YearCur->CLass[i].Stu[j].Name;
+			output >> YearCur->CLass[i].Stu[j].DOB;
+			output >> YearCur->CLass[i].Stu[j].Gender;
+			output >> YearCur->CLass[i].Stu[j].StudentID;	
+		}
+	}
+
+	output.close();
+	ViewListStudentInClass(YearCur);
 }
